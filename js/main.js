c9591f622bf6910fa32bde90153e0f32e92d2c83
Alabi johnson <abiodunalabijohn@gmail.com>
2019-07-10 14:14:11 +0100

    new

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-$('.nav-link').on('click', function(e){
-    e.preventDefault();
-    $(this).css('border-color', 'lime');
-  });
+// $('.nav-link').on('click', function(e){
+//     e.preventDefault();
+//     $(this).css('border-color', 'lime');
+//   });
+
+
+function btnincre() {}
+
+function subone() {
+    var foo = document.getElementById('count-num').innerHTML
+    foo++;
+    document.getElementById('count-num').innerHTML=foo;
+}
