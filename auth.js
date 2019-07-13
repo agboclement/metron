d958f390b56762689053c61166ff913c0b58b560
Donifez <donifez224@gmail.com>
2019-07-13 01:55:11 -0700

    storing

--- auth.js
+++ auth.js
@@ -5,5 +5,12 @@ signupForm.addEventListener('submit',(e)=>{
 const fullname= signupForm['signupfullname'].value;
     const email= signupForm['signupemail'].value;
     const password= signupForm['signuppassword'].value;
-    console.log(fullname,email,password)
+ 
+//sign up the user
+auth.createUserWithEmailAndPassword(email, password).then( cred=> {
+    console.log(cred)
+    signupForm.reset();
+})
+
+
 })
